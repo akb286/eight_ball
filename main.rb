ab7cbbcf4313be2307728f4a0b74f3582bb9a461
Alex Shapiro <acjshapiro@gmail.com>
2018-02-09 14:35:27 -0700

    Initial Commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+require 'answers'
+
+def menu
+  puts "I am an eight ball, yo."
+  puts "Please enter your question..."
+  response = gets.strip
+end
+
+answers = [
+  "I'll think about it... hmmmm.  I dunno. but you're a jibroni.",
+  "You should probably stop eating tide pods.",
+  "Thats an affirmative.",
+  "Thats gonna be a hard No.",
+  
+
+
+]
