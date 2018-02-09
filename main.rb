88d4a5f0adbc0c23592905a5d5c7b5b82aea1f7c
Alex Shapiro <acjshapiro@gmail.com>
2018-02-09 14:48:28 -0700

    updates

--- main.rb
+++ main.rb
@@ -11,8 +11,7 @@ answers = [
   "You should probably stop eating tide pods.",
   "Thats an affirmative.",
   "Thats gonna be a hard No.",
-  "Don't count on it!",
-  "You would be better off going to medical school!",
-  "You betcha.",
-  "My sources say NO, but they also said Hilary would win."
+
+
+
 ]
